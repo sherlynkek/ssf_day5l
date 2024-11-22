a91eb1788b0a887b7f5f479e93d431cdca82e8c0
sherlynkek <sherlynkek00@gmail.com>
2024-11-22 11:04:26 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/vttp5a_day5l/Vttp5aDay5lApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_day5l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aDay5lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
